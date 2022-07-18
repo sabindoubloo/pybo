18a9fffbea86de94e0d10b9669e8061b1aaa75cf
문사빈 <sabindoubleoo@gmail.com>
2022-07-18 17:39:07 +0900

    파이보 프로젝트 최초 커밋

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from pybo import views
+from pybo.views import base_views
+
+urlpatterns = [
+    path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('admin/', admin.site.urls),
+    path('', base_views.index, name='index'),
+]
